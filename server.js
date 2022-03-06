aa9eeab12b4881eb59036e17e73f4a2b97db8cfe
Josh Evans <88986229+Jevans1441@users.noreply.github.com>
2022-03-06 17:02:43 -0500

    add ui boilerplate code with create-react-app

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require("express");
 
 const server = express();
 
+server.use(express.static(path.resolve(__dirname + "/react-ui/build")));
+
 server.get("/heartbeat", (req, res) => {
   res.json({
     is: "working",
   });
+});
+
+server.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "./react-ui/build/index.html"));
 });
 
 server.listen("8080", () => {
