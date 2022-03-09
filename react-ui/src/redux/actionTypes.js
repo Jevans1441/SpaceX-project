9f820249fa951df897831e7f5532dc31c12b3a62
Josh Evans <88986229+Jevans1441@users.noreply.github.com>
2022-03-08 19:53:38 -0500

    add redux packages, set up rocket action

---
+++ react-ui/src/redux/actionTypes.js
@@ -0,0 +1,1 @@
+export const GET_ROCKETS = "GET_ROCKETS";
---
+++ react-ui/src/redux/actions.js
@@ -0,0 +1,25 @@
+import { GET_ROCKETS } from "./actionTypes";
+
+const ROCKET = "https://api.spacexdata.com/v4/rockets";
+
+export const getRockets = () => {
+  try {
+    return async (dispatch) => {
+      const result = await fetch(ROCKET, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const json = await result.json();
+      if (json) {
+        dispatch({
+          type: GET_ROCKETS,
+          payload: json,
+        });
+      } else {
+        console.log("Unable to Fetch");
+      }
+    };
+  } catch (error) {}
+};
